fd5e556098b56dcf38f782ebb8c840eebc51886c
BryKafka <bryan.lume@edu.itspiemonte.it>
2024-07-18 11:42:18 +0200

    mvc

--- PizzeraIDerubati/src/main/java/com/gruppo/controllers/MVC.java
+++ PizzeraIDerubati/src/main/java/com/gruppo/controllers/MVC.java
@@ -1,5 +1,14 @@
 package com.gruppo.controllers;
 
 public class MVC {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class MVC {
+	
+	@GetMapping("")
+	String home() {
+		return "pizze";
+	}
 }
