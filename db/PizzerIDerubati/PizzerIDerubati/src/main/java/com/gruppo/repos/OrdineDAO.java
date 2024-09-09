c9ad19980daaf312579bbfb1c5f406aadc021e60
fornou <mattiaforneron@gmail.com>
2024-09-09 10:25:50 +0200

    Mod

--- db/PizzerIDerubati/PizzerIDerubati/src/main/java/com/gruppo/repos/OrdineDAO.java
+++
@@ -1,7 +0,0 @@
-package com.gruppo.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gruppo.entities.Ordine;
-
-public interface OrdineDAO extends JpaRepository<Ordine, Integer> {}
